d15389f3c78cace1a84e098f1480a60e1b721890
Oleh <krupao980@gmail.com>
2023-12-26 10:31:11 +0200

    React 17 -> 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import App from './components/app/App';
+
+import MarvelService from './services/MarvelService';
 import './style/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const marvelService = new MarvelService();
 
+marvelService.getAllCharacters().then(res => console.log(res));
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
