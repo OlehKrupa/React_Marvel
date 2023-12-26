b07bc397b195bf3b9009cd8a2c829685a0ebab85
Oleh <krupao980@gmail.com>
2023-12-26 18:04:24 +0200

    Pagination/Load more

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=a4cfb58e1c298ea73cd9bdd728ab9e24';
+    _baseOffset = 210;
+    _baseLimit = 9;
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -12,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
