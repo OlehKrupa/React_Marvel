a09d98642f0cfdb58560549ec2135675fea9fa1c
Oleh <krupao980@gmail.com>
2023-12-28 11:58:33 +0200

    React router 5 -> 6

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import {MainPage, ComicsPage} from '../pages/Index';
+import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages/Index';
 import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
@@ -9,15 +9,12 @@ const App = () => {
             <div className="app">
             <AppHeader/>
             <main>
-                <Switch>
-                    <Route exact path="/">
-                        <MainPage/>
-                    </Route>
-
-                    <Route exact path="/comics">
-                        <ComicsPage/>
-                    </Route>
-                </Switch>
+                <Routes>
+                    <Route exact path="/" element={<MainPage/>} />
+                    <Route exact path="/comics" element={<ComicsPage/>} />
+                    <Route exact path="/comics/:comicId" element={<SingleComicPage/>} />
+                    <Route path='*' element={<Page404/>}/>
+                </Routes>
             </main>
         </div>
         </Router>
